32ff7237c6ad2c8620ceccc066f6eac893a47701
Jazmin Gonzalez <gonzalez.jazmin@student.greenriver.edu>
2023-02-15 16:32:01 -0500

    Created arrays in controller and worked with tokens

--- index.php
+++ index.php
@@ -12,7 +12,34 @@ require_once('vendor/autoload.php');
 $f3 = Base::instance();
 
 // define a default route (SDEV328/week6)
-$f3->route('GET /', function() {
+$f3->route('GET /', function($f3) {
+
+    $f3->set('username', 'jshmo');
+    $f3->set('password', sha1('Password01'));
+    $f3->set('title', 'Working with templates');
+
+    //Add your fav food and color to the hive
+    $f3->set('food', 'pozole');
+    $f3->set('color', 'lavender');
+
+    //radius variable
+    $f3->set('radius', '10');
+
+    //define an array of fruits
+    $fruits = array("apple", "banana", "orange");
+    //put this array into our hive
+    $f3->set('fruits', $fruits);
+    //for each loops
+    //     array name |  placeholder for each element in our array
+    /*foreach ($fruits as $fruit){
+        echo $fruit;
+    }*/
+
+    //creating an array of colors
+    $f3->set('colors', array("red", "blue", "pink"));
+
+
+
     //instantiate a view
     $view = new Template(); // template is a fat free class
     echo $view->render("views/info.html"); // render method, return text on template
